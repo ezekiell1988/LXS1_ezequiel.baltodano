93eefe835f5acb84fcabaabd4985431534f05f4c
nombre_usuario <email_id>
2017-09-23 21:28:19 +0000

    Agrego solicitud de información del usuario

--- src/tmp_script.sh
+++ src/tmp_script.sh
@@ -3,12 +3,23 @@ GREEN="\033[0;32m"
 RED="\033[0;31m"
 YELLOW="\033[0;33m"
 RESET="\033[0m"
-#h Revisar que sea el usuario root
-if [[ 0 != $UID ]]
+# Revisar que el usuario no sea  root
+if [[ 0 == $UID ]]
 then
-	echo -e "${RED}Usted debe ser Root para correr este script${RESET}" #${RED} uso la variable de entorno
+	echo -e "${RED}Usted no debe ser Root para correr este script${RESET}" #${RED} uso la variable de entorno
 	exit 1
 fi
+#Obtener Información
+echo -en "Escriba su nombre completo > "
+read NOMBRE
+echo -en "Escriba su correo electronico > "
+read EMAIL
 
-echo Hola $USER
+echo -en "Hola $NOMBRE, este es su correo [${GREEN}$EMAIL${RESET}]? [s/n]: "
+read RESP
+
+if [[ s ==  $RESP ]]
+then
+	echo Hola $NOMBRE, su correo es $EMAIL
+fi
 
